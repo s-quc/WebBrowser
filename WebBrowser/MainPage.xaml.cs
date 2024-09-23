4a22129332b0e4aad60c13c80319c66f2556a244
Collin Qu <collinqu@gmail.com>
2024-09-23 13:21:28 -0700

    current added stackpanel

--- WebBrowser/MainPage.xaml.cs
+++ WebBrowser/MainPage.xaml.cs
@@ -24,12 +24,12 @@ namespace WebBrowser
     
     public sealed partial class MainPage : Page
     {
+        private List<Uri> favorites = new List<Uri>();
         public String prevURL = "";
         public Uri uri = new System.Uri("https://google.com");
         public MainPage()
         {
             this.InitializeComponent();
-            
         }
 
         private void WebView_LoadCompleted(object sender, NavigationEventArgs e)
@@ -39,19 +39,72 @@ namespace WebBrowser
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            wvMain.Navigate(uri);
-            //wvMain.Navigate(new Uri("https://google.com"));
+            //wvMain.Navigate(uri);
+            wvMain.Navigate(new Uri("https://google.com"));
         }
 
         private void bBack_Click(object sender, RoutedEventArgs e)
         {
             //wvMain.Navigate(new System.Uri(prevURL));
-            wvMain.GoBack();
+            if (wvMain.CanGoBack)
+            {
+                wvMain.GoBack();
+            }
+
+            /*String url = wvMain.Source.ToString();
+            string url = wvMain.Source.AbsoluteUri;
+            wvMain.Source = new Uri(url);
+            wvMain.Navigate(new System.Uri(url));*/
         }
 
         private void bForward_Click(object sender, RoutedEventArgs e)
         {
-            wvMain.GoForward();
+            if (wvMain.CanGoForward) 
+            {
+                wvMain.GoForward();
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (wvMain.Source is Uri currentUri)
+            {
+                if (!favorites.Contains(currentUri))
+                {
+                    favorites.Add(currentUri);
+                    CreateFavoriteButton(currentUri);
+                }
+            }
+        }
+
+
+        private void CreateFavoriteButton(Uri uri)
+        {
+            //String display = uri.AbsoluteUri;
+            //display.Substring(display.IndexOf(:/
+            Button favoriteButton = new Button
+            {
+                
+                Content = uri.AbsoluteUri,
+                Tag = uri
+            };
+            favoriteButton.Click += FavoriteButton_Click;
+
+            FavoritesStack.Children.Add(favoriteButton);
+        }
+
+        private void FavoriteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button && button.Tag is Uri uri)
+            {
+                wvMain.Navigate(uri);
+            }
+        }
+
+        private void EditFavorite_Click(object sender, RoutedEventArgs e)
+        {
+            //When the user clicks on a favorite, they can change the name
+            //They should also be able to delete the favorite
         }
     }
 }
